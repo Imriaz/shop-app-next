7b2bda25c3ee9d08d719d806ce2fbe5ce58e08dc
Imriaz <imriaz.cse.bubt@gmail.com>
2023-01-31 17:10:28 +0600

    style added

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div style={{display:'flex', gap:'10px'}}>
+        <div className='navbar' style={{display:'flex', gap:'10px'}}>
             <Link href='/'>Home</Link>
             <Link href='/about'>About</Link>
             <Link href='/product-list'>Product List</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Navbar />
-        
-        <Footer />
       </main>
     </>
   )
