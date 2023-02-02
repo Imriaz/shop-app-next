d5d4f690f68bea751735d74735a1623cb054adf4
Imriaz <imriaz.cse.bubt@gmail.com>
2023-02-02 12:11:40 +0600

    Create customize 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,6 +5,8 @@ const NotFound = () => {
     return (
         <div className='not-found'>
             <h1>404! Not Found</h1>
+            <img src="https://i.ibb.co/CP5p6Rn/404-error.jpg" alt="404 not found" />
+            <br />
             <p>Go back to <Link href="/">Homepage</Link></p>
         </div>
     );
