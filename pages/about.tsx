b268024b2448cd55cdf481c980ae80dfb3d6fe6d
Imriaz <imriaz.cse.bubt@gmail.com>
2023-02-02 14:27:25 +0600

    Add metadata & custom title

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,17 @@
+import Head from 'next/head';
 import React from 'react';
 
 const about = () => {
     return (
+        <>
+        <Head>
+        <title>Shop App | About</title>
+        <meta name="about" content="About page created by create next app" />
+      </Head>
         <div>
             <h1>About</h1>
         </div>
+        </>
     );
 };
 
