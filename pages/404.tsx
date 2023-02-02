f14fea6eebd1b4524e97951e24607a4e1c8192b0
Imriaz <imriaz.cse.bubt@gmail.com>
2023-02-02 12:32:51 +0600

    Redirect automatically after certain time

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+const router = useRouter();
+    React.useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
     return (
         <div className='not-found'>
             <h1>404! Not Found</h1>
