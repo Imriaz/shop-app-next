e2e966f2544f777460cefce679946d7085114830
Imriaz <imriaz.cse.bubt@gmail.com>
2023-02-02 14:12:13 +0600

    image added on this projects

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <div className='navbar' style={{display:'flex', gap:'10px'}}>
-            <Link href='/'>Home</Link>
-            <Link href='/about'>About</Link>
-            <Link href='/product-list'>Product List</Link>
-            <Link href='/Contact'>Contact Us</Link>
-            <Link href='/Registration'>Registration</Link>
-            <Link href='/Login/Login'>Login</Link>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div>
+        <Image src="/logo.png" height={50} width={100} alt="Logo" />
+      </div>
+      <div className="navbar" style={{ display: "flex", gap: "10px" }}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/product-list">Product List</Link>
+        <Link href="/Contact">Contact Us</Link>
+        <Link href="/Registration">Registration</Link>
+        <Link href="/Login/Login">Login</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
